5dde2a072d4c10dc771fa31fa9514555b4c50da7
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-22 11:41:07 +0200

    chore: Update favicon and CSS output file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "hsl(47, 88%, 63%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: "hsl(0, 0%, 50%)",
+        black: "hsl(0, 0%, 7%)",
+      },
+      fontFamily: {
+        figtree: ["Figtree", "serif"],
+      },
+      fontWeight: {
+        medium: 500,
+        "extra-bold": 800,
+      },
+    },
   },
   plugins: [],
-}
-
+};
